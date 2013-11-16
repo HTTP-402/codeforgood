82540279ce941721a23a45802cdf322ec0331df6
Hans-Nikolai Viessmann <hansthespass@googlemail.com>
2013-11-16 18:19:42 +0000

    Added parser
    The parser currently converts empty comments to 'none' and the meal-type
    and reference to 'err' if they are empty.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ codeforgood
 ===========
 
 Main Application for the J.P.Morgan Code for Good Challenge
+
+parser
+------
+
+Takes in a CSV file (of specific format) and outputs an associative array.
---
+++ parser/index.php
@@ -0,0 +1,78 @@
+<?php
+function csv_to_array($filename='', $delimiter=',')
+{
+    if(!file_exists($filename) || !is_readable($filename)){
+		echo "File not read";
+        return FALSE;
+	}
+
+    $header = NULL;
+	$entry = array();
+	$prev = array();
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 500, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = $row;
+            else {
+				$entry = make_array($header, $row);
+				if(!empty($entry['Nickname'])){
+					if(!empty($prev))
+						$data[] = $prev;
+					$prev = $entry;
+				} else {
+					array_push($prev['Comments'], $entry['Comments'][0]);
+					array_push($prev['Meal Type'], $entry['Meal Type'][0]);
+					array_push($prev['Booking ID'], $entry['Booking ID'][0]);
+				}
+			}
+        }
+        fclose($handle);
+    }
+    return $data;
+}
+
+function make_array($headers, $row)
+{
+	$array = array();
+	foreach($headers as $key => $value)
+	{
+		switch($value){
+		case 'Kitchen':
+			$array[$value] = $row[$key];
+			break;
+		case 'Notes from kitchen':
+			$array[$value] = $row[$key];
+			break;
+		case 'Route and Code':
+			$array[$value] = $row[$key];
+			break;
+		case 'Birthdate':
+			$array[$value] = $row[$key];
+			break;
+		case 'Meal Type':
+			$array[$value] = array(empty($row[$key]) ? 'err' : $row[$key]);
+			break;
+		case 'Booking ID':
+			$array[$value] = array(empty($row[$key]) ? 'err' : $row[$key]);
+			break;
+		case 'Comments':
+			$array[$value] = array(empty($row[$key]) ? 'none' : $row[$key]);
+			break;
+		case 'Cake?':
+			$array[$value] = empty($row[$key]) ? 'no' : $row[$key];
+			break;
+		default:
+			$array[$value] = $row[$key];
+			break;
+		}
+	}
+	return $array;
+}
+
+echo "Hello";
+$test = csv_to_array("service-users.csv");
+print_r($test);
+?>
