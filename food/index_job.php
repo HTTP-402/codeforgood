6545f32eff83b13dbddb8bb929593ce0c9b4771d
Roisin Farmer <rlf6@hw.ac.uk>
2013-11-17 08:24:08 +0000

    hardcode populate kitchens

--- food/index_job.php
+++ food/index_job.php
@@ -60,10 +60,10 @@ function calcRoute() {
 </form>						<div class="menu-main-menu-container"><ul id="menu-main-menu" class="nav-menu-primary"><li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children has-children menu-item-30"><a href="http://localhost.com/jobify/jobs/">Kitchens</a>
 <ul class="sub-menu" onclick="updateroutes();">
 	<!--populate with kitchens from database--> 
-    <li id="menu-item-2054" ><a href="http://localhost.com/jobify/job-region/palo-alto/">Palo Alto</a></li>
-	<li id="menu-item-2055" ><a href="http://localhost.com/jobify/job-region/san-francisco/">San Francisco</a></li>
-	<li id="menu-item-2056" ><a href="http://localhost.com/jobify/job-region/san-jose/">San Jose</a></li>
-	<li id="menu-item-2053" ><a href="http://localhost.com/jobify/job-region/mountain-view/">Mountain View</a></li>
+    <li id="0" ><a href="http://localhost.com/jobify/job-region/palo-alto/">Albrighton Community Centre</a></li>
+	<li id="1" ><a href="http://localhost.com/jobify/job-region/san-francisco/">Acorn House</a></li>
+	<li id="2" ><a href="http://localhost.com/jobify/job-region/san-jose/">Highgate Centre</a></li>
+	
 
 </ul>
 </li>
